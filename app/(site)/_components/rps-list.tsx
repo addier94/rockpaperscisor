4d1c05183d1d0ada62b4370f8c9231953ff6a37f
alfred <alfredofernandezcarlos@gmail.com>
2023-12-18 11:13:07 -0400

    add header ui

---
+++ app/(site)/_components/rps-list.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image"
+
+export const RpsList = () => {
+  return (
+    <>
+      <header className="
+        border-[3px] 
+        border-neutral-header-outline
+        pl-5
+        py-[.62rem]
+        pr-[.62rem]
+        h-[6.2rem]
+        rounded-md
+        flex
+        items-center
+        justify-between
+      ">
+        <figure className="w-[5.4rem]">
+          <Image 
+            src={"/images/logo.svg"}
+            width="0"
+            height="0"
+            alt="Rock Paper Scissors Logo"
+            sizes="100vw"
+            className="w-full"
+            priority
+          />
+        </figure>
+        <p className="
+          flex 
+          flex-col 
+          items-center
+          justify-center
+          w-[4.8rem]
+          h-full
+          bg-white
+          rounded-md
+          font-700
+        ">
+          <span className="
+            text-xs
+            text-neutral-score-text">SCORE</span>
+          <span className="
+            text-4xl
+            text-neutral-dark-text">12</span>
+        </p>
+      </header>
+    </>
+  )
+}
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,19 @@
+import { RpsList } from "./_components/rps-list";
 
 export default function Home() {
   return (
-    <div>
-      home page
+    <div 
+      className="
+        bg-gradient-to-b 
+        from-background-radial-gradient-start 
+        to-background-radial-gradient-end
+        min-h-screen
+        text-white
+        py-8
+        px-8
+      "
+    >
+      <RpsList />
     </div>
   )
 }
